b1e843e032048a1efe4ca352971eaec83380c797
Oliver Hensworth <oliverhensworth1@gmail.com>
2019-03-01 17:49:45 +0000

    create examples

---
+++ examples/gettingGroups.rb
@@ -0,0 +1,3 @@
+require "./init" # of course, you will reference this init module.
+
+puts ROBLOX.getGroup(1214735)
---
+++ examples/gettingUsers.rb
@@ -0,0 +1,3 @@
+require "./init" # of course, you will reference this init module.
+
+puts ROBLOX.getUser(1214735)
